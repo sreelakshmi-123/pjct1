c903d2280be65f92300c3c79a54fccede05442c8
sreelakshmi-123 <57605317+sreelakshmi-123@users.noreply.github.com>
2021-05-17 23:02:03 +0530

    initialcommit

---
+++ action.js
@@ -0,0 +1,14 @@
+function purple(){
+ document.body.style.background="purple"
+
+}
+function green(){
+    document.body.style.background="green"
+
+    
+}
+function yellow(){
+    document.body.style.background="yellow"
+
+    
+}
